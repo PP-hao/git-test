b5420cba0af5b2ba800951e33cc0ccb7d1af3d66
ppdfn <2695691064@qq.com>
2023-03-18 09:09:14 +0800

    first commit

---
+++ src/main/java/com/pang/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.pang.git;
+
+/**
+ * @Author shenzhuanghao
+ * @Date 2023/3/18 8:50
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
